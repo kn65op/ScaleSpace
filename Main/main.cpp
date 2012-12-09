9a37aea6f3fadd7f05581143cb62f80d6ae37a18
Tomasz Drzewiecki <kn65op@gmail.com>
2012-12-09 11:42:58 +0100

    initial commit

---
+++ Main/main.cpp
@@ -0,0 +1,40 @@
+#include <OpenCLGaussian.h>
+#include <OpenCLDevice.h>
+
+#include <opencv\cv.h>
+#include <opencv\highgui.h>
+
+#include <iostream>
+
+int main()
+{
+  try
+  {
+    OpenCLDevice device = OpenCLDevice::getDevices().front();
+
+    OpenCLGaussian gaussian;
+
+    gaussian.setDevice(device);
+
+    gaussian.prepare();
+
+    cv::Mat input, input_tmp;
+    input_tmp = cv::imread("in.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+    input_tmp.convertTo(input, CV_32F, 1.0/255.0f);
+    
+    cv::Mat output_tmp(input.rows - 2, input.cols - 2, input.type());
+    
+    gaussian.run(input.data, input.total() * input.elemSize(), output_tmp.data, output_tmp.total() * output_tmp.elemSize());
+
+    cv::Mat output;
+
+    output_tmp.convertTo(output, CV_8U, 255);
+    cv::imwrite("out.bmp", output);
+  }
+  catch (OpenCLException ex)
+  {
+    std::cout << ex.getFullMessage() << "\n";
+  }
+
+  return 0;
+}
