4a87555f33240af5f0f3d47222eb3ca6596f53c9
Tomasz Drzewiecki <kn65op@gmail.com>
2013-03-27 18:39:40 +0100

    Changes due to changes in OpenCLpp

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,4 +1,6 @@
 #include <OpenCLGaussian.h>
+#include <OpenCLFloatToInt.h>
+#include <OpenCLIntToFloat.h>
 #include <OpenCLAlgorithmsStream.h>
 
 #include <opencv\cv.h>
@@ -13,22 +15,27 @@ int main()
   {
     OpenCLAlgorithmsStream gaussian_stream;
     OpenCLImageAlgorithm *gaussian = new OpenCLGaussianImage();
+    OpenCLImageAlgorithm *itf = new OpenCLIntToFloat();
+    OpenCLImageAlgorithm *fti = new OpenCLFloatToInt();
 
-    cv::Mat gaussian_kernel = cv::getGaussianKernel(400, -1, CV_32F);
+    cv::Mat gaussian_kernel = cv::getGaussianKernel(9, -1, CV_32F);
 
     OpenCLGaussianParams params;
-    params.setMask(20, gaussian_kernel.data);
+    params.setMask(3, gaussian_kernel.data);
     gaussian->setParams(params);
 
+    gaussian_stream.pushAlgorithm(itf);
     gaussian_stream.pushAlgorithm(gaussian);
+    gaussian_stream.pushAlgorithm(fti);
     
     OpenCLDevice device = OpenCLDevice::getDevices().front();
 
     cv::Mat input, input_tmp;
-    input_tmp = cv::imread("in.bmp", CV_LOAD_IMAGE_GRAYSCALE);
-    input_tmp.convertTo(input, CV_32F, 1.0/255.0f);
+    input = cv::imread("in.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+    //input.convertTo(input_tmp, CV_32F, 1.0/255.0f);
     
     cv::Mat output(input.size(), input.type());
+    //cv::Mat output(input_tmp.size(), input_tmp.type());
     
     gaussian_stream.setDataSize(input.size().width, input.size().height);
     
@@ -38,10 +45,14 @@ int main()
 
     gaussian_stream.processImage(input.data, output.data);
 
-    std::cout << output.at<float>(0,0) << "\n";
+    //std::cout << output.at<int>(0,0) << "\n";
     std::cout << gaussian_stream.getTime() << "\n";
-    output.convertTo(output, CV_8U, 255.0);
+    //output.convertTo(output, CV_8U, 255.0);
     cv::imwrite("out.bmp", output);
+  }
+  catch (OpenCLDeviceException ex)
+  {
+    std::cout << ex.getFullMessage() << "\n" << ex.getSource() << "\n";
   }
   catch (OpenCLException ex)
   {
