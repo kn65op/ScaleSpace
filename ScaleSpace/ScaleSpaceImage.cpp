c550c21ace9c035a4ec70a6c0138b607a672be26
Tomasz Drzewiecki <kn65op@gmail.com>
2013-09-09 19:05:57 +0200

    #34 errata

--- ScaleSpace/ScaleSpaceImage.cpp
+++ ScaleSpace/ScaleSpaceImage.cpp
@@ -98,8 +98,15 @@ void ScaleSpaceImage::show(std::string prefix, std::string processor, unsigned i
     {
       std::string ds1 = s + "_1.bmp";
       std::string ds2 = s + "_2.bmp";
+      std::string ds1txt = s + "_1.txt";
+      std::string ds2txt = s + "_2.txt";
       scale_space_images[0][i].convertTo(to_write, CV_8UC1, 255.0);
       cv::imwrite(ds1.c_str(), to_write);
+
+      std::ofstream im1(ds1txt);
+      std::ofstream im2(ds2txt);
+      im1 << scale_space_images[0][i];
+      im2 << scale_space_images[1][i];
 
       scale_space_images[1][i].convertTo(to_write, CV_8UC1, 255.0);
       cv::imwrite(ds2.c_str(), to_write);
